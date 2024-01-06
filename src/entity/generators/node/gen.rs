f60b235fa32920986473c131d13dad2a63746d86
Victor Teo <victor.teo@fillmula.com>
2024-01-06 12:15:08 +0800

    add nodejs code

--- src/entity/generators/node/gen.rs
+++ src/entity/generators/node/gen.rs
@@ -1,20 +1,46 @@
+use askama::Template;
 use async_trait::async_trait;
+use teo_result::Result;
+use teo_runtime::namespace::Namespace;
 use crate::entity::ctx::Ctx;
 use crate::entity::generator::Generator;
+use crate::shared::ts::conf::TsConf;
+use crate::shared::ts::templates::{render_namespace, TsIndexDTsTemplate};
 use crate::utils::file::FileUtil;
+
+#[derive(Template)]
+#[template(path = "entity/nodejs/index.js.jinja", escape = "none")]
+pub(self) struct TsIndexJsTemplate { }
 
 pub(crate) struct NodeGenerator {}
 
 impl NodeGenerator {
+
     pub fn new() -> Self {
         Self {}
+    }
+
+    pub async fn generate_index_js(&self, generator: &FileUtil) -> Result<()> {
+        generator.generate_file("index.js", TsIndexJsTemplate { }.render().unwrap()).await
+    }
+
+    pub async fn generate_index_d_ts(&self, main_namespace: &Namespace, generator: &FileUtil) -> Result<()> {
+        generator.generate_file("index.d.ts", TsIndexDTsTemplate {
+            main_namespace,
+            conf: &TsConf::new("teo".to_string(), "Teo".to_string(), false),
+            render_namespace: &render_namespace,
+        }.render().unwrap()).await
     }
 }
 
 #[async_trait]
 impl Generator for NodeGenerator {
 
-    async fn generate_entity_files(&self, ctx: &Ctx, generator: &FileUtil) -> teo_result::Result<()> {
+    async fn generate_entity_files(&self, ctx: &Ctx, generator: &FileUtil) -> Result<()> {
+        // index.js
+        self.generate_index_js(generator).await?;
+        // index.d.ts
+        self.generate_index_d_ts(ctx.main_namespace, generator).await?;
         Ok(())
     }
 }
