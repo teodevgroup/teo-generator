e5608ef0368924db8f3bdf4f6741e18ffdeef2ab
Victor Teo <victor.teo@fillmula.com>
2024-04-01 08:36:49 +0800

    generate admin method

---
+++ src/admin/mod.rs
@@ -0,0 +1,26 @@
+use teo_runtime::config::admin::Admin;
+use teo_runtime::namespace::Namespace;
+use serde::Deserialize;
+use crate::utils::file::FileUtil;
+
+static FILE_ADDRESS: &'static str = "https://raw.githubusercontent.com/teocloud/teo-admin-dev/main/";
+static FILE_JSON: &'static str = ".generator/data/fileList.json";
+
+#[derive(Deserialize)]
+struct FileList {
+    generated: Vec<String>,
+    extended: Vec<String>,
+}
+
+pub async fn generate(main_namespace: &Namespace, admin: &Admin) -> teo_result::Result<()> {
+    let file_util = FileUtil::new(admin.dest.as_str());
+    file_util.ensure_root_directory().await?;
+    let file_list = reqwest::get(FILE_ADDRESS.to_owned() + FILE_JSON)
+        .await?
+        .json::<FileList>()
+        .await?;
+    for generated in &file_list.generated {
+        println!("generated {generated}");
+    }
+    Ok(())
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 pub mod client;
 pub mod entity;
+pub mod admin;
 pub mod outline;
 pub(crate) mod shared;
 pub(crate) mod utils;
