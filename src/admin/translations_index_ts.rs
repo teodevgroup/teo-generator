f605a7f52da26b6a4e65eeff1e9eeae4ff13e2c3
Victor Teo <victor.teo@fillmula.com>
2024-04-29 22:58:15 +0800

    generate translations

--- src/admin/translations_index_ts.rs
+++ src/admin/translations_index_ts.rs
@@ -1,6 +1,78 @@
+use askama::Template;
+use inflector::Inflector;
+use itertools::Itertools;
 use teo_runtime::namespace::Namespace;
+use teo_result::Result;
+use teo_runtime::traits::documentable::Documentable;
+use teo_runtime::traits::named::Named;
 use crate::utils::file::FileUtil;
+use snailquote::escape;
 
-pub(crate) async fn generate_translations_index_ts(namespace: &Namespace, file_util: &FileUtil) -> teo_result::Result<()> {
+pub(super) struct TranslationEntry {
+    pub(super) key: String,
+    pub(super) value: String,
+}
+
+fn wrap(value: impl AsRef<str>) -> String {
+    "\"".to_owned() + escape(value.as_ref()).to_string().as_str() + "\""
+}
+
+pub(super) fn fetch_translation_entries(namespace: &Namespace, lang: &'static str) -> Vec<TranslationEntry> {
+    let mut result = vec![];
+    let models = namespace.collect_models(|m| m.data.get("admin:ignore").is_none());
+    for model in models {
+        let model_path = model.path().iter().map(|s| s.to_camel_case()).join(".");
+        result.push(TranslationEntry {
+            key: format!("model.{}.name", model_path),
+            value: wrap(&model.title())
+        });
+        result.push(TranslationEntry {
+            key: format!("model.{}.desc", model_path),
+            value: wrap(&model.desc())
+        });
+        for field in model.fields() {
+            result.push(TranslationEntry {
+                key: format!("model.{}.{}.name", model_path, field.name()),
+                value: wrap(&field.title()),
+            });
+            result.push(TranslationEntry {
+                key: format!("model.{}.{}.desc", model_path, field.name()),
+                value: wrap(&field.desc()),
+            });
+        }
+        for relation in model.relations() {
+            result.push(TranslationEntry {
+                key: format!("model.{}.{}.name", model_path, relation.name()),
+                value: wrap(&relation.title()),
+            });
+            result.push(TranslationEntry {
+                key: format!("model.{}.{}.desc", model_path, relation.name()),
+                value: wrap(&relation.desc()),
+            });
+        }
+        for property in model.properties() {
+            result.push(TranslationEntry {
+                key: format!("model.{}.{}.name", model_path, property.name()),
+                value: wrap(&property.title()),
+            });
+            result.push(TranslationEntry {
+                key: format!("model.{}.{}.desc", model_path, property.name()),
+                value: wrap(&property.desc()),
+            });
+        }
+    }
+    result
+}
+
+#[derive(Template)]
+#[template(path = "admin/lib/generated/translations/index.ts.jinja", escape = "none")]
+pub(self) struct TranslationsIndexTsTemplate {
+    pub(self) entries: Vec<TranslationEntry>,
+}
+
+pub(crate) async fn generate_translations_index_ts(namespace: &Namespace, file_util: &FileUtil) -> Result<()> {
+    file_util.ensure_directory_and_generate_file("src/lib/generated/translations/index.ts", TranslationsIndexTsTemplate {
+        entries: fetch_translation_entries(namespace, "enUs")
+    }.render().unwrap()).await?;
     Ok(())
 }
--- src/admin/translations_lang_index_ts.rs
+++ src/admin/translations_lang_index_ts.rs
@@ -1,6 +1,18 @@
 use teo_runtime::namespace::Namespace;
 use crate::utils::file::FileUtil;
+use askama::Template;
+use teo_result::Result;
+use crate::admin::translations_index_ts::{fetch_translation_entries, TranslationEntry};
 
-pub(crate) async fn generate_translations_lang_index_ts(lang: &'static str, namespace: &Namespace, file_util: &FileUtil) -> teo_result::Result<()> {
+#[derive(Template)]
+#[template(path = "admin/lib/generated/translations/lang/index.ts.jinja", escape = "none")]
+pub(self) struct TranslationsLangIndexTsTemplate {
+    pub(self) entries: Vec<TranslationEntry>,
+}
+
+pub(crate) async fn generate_translations_lang_index_ts(lang: &'static str, namespace: &Namespace, file_util: &FileUtil) -> Result<()> {
+    file_util.ensure_directory_and_generate_file(&format!("src/lib/generated/translations/{}/index.ts", lang), TranslationsLangIndexTsTemplate {
+        entries: fetch_translation_entries(namespace, lang)
+    }.render().unwrap()).await?;
     Ok(())
 }
