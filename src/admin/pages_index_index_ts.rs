2f17397e0aa19909495908a81ee6aa3c90e9c6f1
Victor Teo <victor.teo@fillmula.com>
2024-05-03 03:20:11 +0800

    fix

--- src/admin/pages_index_index_ts.rs
+++ src/admin/pages_index_index_ts.rs
@@ -2,6 +2,6 @@ use teo_runtime::namespace::Namespace;
 use crate::utils::file::FileUtil;
 
 pub(crate) async fn generate_pages_index_index_ts(_namespace: &Namespace, file_util: &FileUtil) -> teo_result::Result<()> {
-    file_util.ensure_directory_and_generate_file("src/components/generated/pages/_Index/index.tsx", include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "templates/admin/components/generated/pages/_Index/index.ts.jinja"))).await?;
+    file_util.ensure_directory_and_generate_file("src/components/generated/pages/_Index/index.tsx", include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/templates/admin/components/generated/pages/_Index/index.ts.jinja"))).await?;
     Ok(())
 }
