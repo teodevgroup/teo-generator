22b8f05256684150488b898320f4cc8e64393f5e
Victor Teo <victor.teo@fillmula.com>
2024-05-12 08:17:47 +0800

    update default preferences

--- src/admin/default_preferences_ts.rs
+++ src/admin/default_preferences_ts.rs
@@ -4,6 +4,7 @@ use itertools::Itertools;
 use teo_runtime::admin::language::Language;
 use teo_runtime::namespace::Namespace;
 use teo_runtime::traits::named::Named;
+use crate::admin::preferences_ts::ModelForPreferences;
 use crate::utils::file::FileUtil;
 
 pub(self) struct NavItem {
@@ -24,11 +25,12 @@ pub(self) struct DefaultPreferencesTsTemplate {
     pub(self) nav_items: Vec<NavItem>,
     pub(self) default_sign_in_model: String,
     pub(self) sign_in_models: Vec<SignInModel>,
+    pub(self) models: Vec<ModelForPreferences>,
 }
 
 pub(crate) async fn generate_default_preferences_ts(namespace: &Namespace, languages: &Vec<Language>, file_util: &FileUtil) -> teo_result::Result<()> {
+    let models = namespace.collect_models(|m| m.data.get("admin:ignore").is_none());
     let sign_in_models = namespace.collect_models(|m| m.data.get("admin:administrator").is_some());
-
     let template = DefaultPreferencesTsTemplate {
         default_lang: languages.first().unwrap().as_str().to_string(),
         nav_items: namespace.collect_models(|m| m.data.get("admin:ignore").is_none()).iter().map(|m| NavItem {
@@ -40,6 +42,11 @@ pub(crate) async fn generate_default_preferences_ts(namespace: &Namespace, langu
             camelcase_name: m.path().iter().map(|s| s.to_camel_case()).join("."),
             default_id_key: m.fields().iter().find(|f| f.data.get("identity:id").is_some()).map_or("".to_owned(), |f| f.name().to_string()),
             default_checker_key: m.fields().iter().find(|f| f.data.get("identity:checker").is_some()).map_or("".to_owned(), |f| f.name().to_string()),
+        }).collect(),
+        models: models.iter().map(|m| ModelForPreferences {
+            key_name: m.path().join("."),
+            var_name: m.path().iter().map(|m| m.to_pascal_case()).join(""),
+            fields: m.fields().iter().filter(|f| !f.write.is_no_write()).map(|f| f.name().to_string()).collect(),
         }).collect(),
     };
     file_util.ensure_directory_and_generate_file("src/lib/generated/defaultPreferences.ts", template.render().unwrap()).await?;
