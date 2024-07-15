a42d0d421d41867273597b73796e7cf45b66341e
Victor Teo <victor.teo@fillmula.com>
2024-07-15 15:33:21 +0800

    fix

--- src/entity/generators/rust/gen.rs
+++ src/entity/generators/rust/gen.rs
@@ -333,10 +333,7 @@ impl RustGenerator {
 
     async fn generate_module_file(&self, namespace: &Namespace, filename: impl AsRef<Path>, generator: &FileUtil, main_namespace: &Namespace) -> Result<()> {
         let template = RustModuleTemplate::new(namespace, main_namespace);
-        println!("before render");
-        let rendered = template.render().unwrap();
-        println!("after render");
-        generator.generate_file(filename.as_ref(), rendered).await?;
+        generator.generate_file(filename.as_ref(), template.render().unwrap()).await?;
         Ok(())
     }
 
